b50ea8b07842e1f2cb1ae2300debf869a310b008
kamicry <abcvuh@163.com>
2025-11-17 12:40:27 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    serverComponentsExternalPackages: ['canvas'],
+  },
+  images: {
+    domains: ['raw.githubusercontent.com', 'example.com'],
+  },
+  api: {
+    bodyParser: {
+      sizeLimit: '10mb',
+    },
+    responseLimit: '10mb',
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
+//export default nextConfig;
